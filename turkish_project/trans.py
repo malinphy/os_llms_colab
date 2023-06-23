e8bb93c07351dbf811a8ebce55d2ea778fc8d363
malinphy <55249305+malinphy@users.noreply.github.com>
2023-06-23 14:40:17 +0300

    Update trans.py

--- turkish_project/trans.py
+++ turkish_project/trans.py
@@ -20,14 +20,18 @@ def tr2eng(input_text):
     # print(eng_text)
     return eng_text
 
-# def eng2tr(input_text):
-#     trg = "tr"  # source language
-#     src = "en"  # target language
-#     model_name_eng2tr = f"Helsinki-NLP/opus-mt-{src}-{trg}"
-#     model_eng2tr = MarianMTModel.from_pretrained(model_name_eng2tr)
-#     tokenizer_eng2tr = AutoTokenizer.from_pretrained(model_name_eng2tr)
-#     batch = tokenizer_eng2tr([input_text], return_tensors="pt")
-#     generated_ids = model_eng2tr.generate(**batch)
-#     turkish_text = tokenizer_eng2tr.batch_decode(generated_ids, skip_special_tokens=True)[0]
-#     print(turkish_text)
-#     return turkish_text    
+
+
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-tc-big-en-tr")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-tc-big-en-tr")
+from transformers import MarianMTModel, MarianTokenizer
+def eng2tr(english_text):
+    model_name = "Helsinki-NLP/opus-mt-tc-big-en-tr"
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    model = MarianMTModel.from_pretrained(model_name)
+    translated = model.generate(**tokenizer(english_text, return_tensors="pt", padding=True))
+    # print(translated)
+    decoded = []
+    for t in translated:
+        decoded.append(tokenizer.decode(t, skip_special_tokens=True) )
+    print(decoded)
